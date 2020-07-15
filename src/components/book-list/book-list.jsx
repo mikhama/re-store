58d35caab42d3f1a53b9d955a72f378f348c8c91
Dzmitry Tsebruk <mikhama@tut.by>
2020-07-15 15:48:24 +0300

    feat: [12-136] learn how to read data from redux store

--- src/components/book-list/book-list.jsx
+++ src/components/book-list/book-list.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import BookListItem from '../book-list-item';
 
@@ -20,4 +21,8 @@ class BookList extends Component {
   }
 };
 
-export default BookList;
+const mapStateToProps = ({ books}) => ({
+  books,
+});
+
+export default connect(mapStateToProps)(BookList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,16 @@
 const initialState = {
-  books: [],
+  books: [
+    {
+      id: 1,
+      title: 'Speaking JavaScript: An In-Depth Guide for Programmers',
+      author: 'Dr. Axel Rauschmayer',
+    },
+    {
+      id: 2,
+      title: 'Harry Potter and the Sorcerer\'s Stone',
+      author: 'J.K. Rowling',
+    },
+  ],
 };
 
 const reducer = (state = initialState, action) => {
